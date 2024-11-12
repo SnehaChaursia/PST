189c73042cafbf3ba9eeb4dfd26091bc0d57b10e
SnehaChaursia <chaurasiasneha30@gmail>
2024-11-12 22:14:33 +0530

    update

---
+++ JavaScript/DAY 1/first.js
@@ -0,0 +1,40 @@
+//03-09-24
+// basic printing
+console.log ("sneha chaurasia");  // string literals
+console.log("i luv you mummy");
+console.log("2.5");
+console.log(45); // number literals 
+
+
+var age =25;
+console.log(age);
+var Age =45;
+console.log(Age);
+// variable are case sensitive
+//  **age and Age treated differently 
+// only bigin with letter,underscore_,dollar$
+// only contain letter,underscore_,dollar$
+// can not be named wth reserved keywords
+
+// var let const are variable which store data in their memory block
+// const 
+// constant are the variable whose values cannot be change 
+// can't re-declared
+// can't re-assigned
+const pi = 3.14;
+console .log(pi);
+
+
+// interpreter :
+// it convert the code written in high level language into machine language which can be understood by computer and directly run in the machine
+// the langauges which use interpreter is known as scripting langauge
+// python, javascript, php........
+
+// About its  History:
+// brendan eich
+// netscape company in 1995 {created in 10 days}
+// mocha > livescript >javascript; 
+// ECMA give standerdatization;
+console.log(8+9) // airthmatic opt.
+
+console.log("7"+5) // 75 treated as string at cocatenate it.
---
+++ JavaScript/DAY 2/variables.js
@@ -0,0 +1,49 @@
+// 04-09-24
+// syntax: it is the way of writing instruction in a particular programming langauge that follows somme set of rules
+// variable :const , let ,var
+let a = 7;
+let b = 7;
+console.log(a+b);
+ a=8;         // let can re -assigned but can't re-declared
+console.log(a)
+
+
+// constant
+const pi = 3.14;
+console.log(pi) // can't reassigned,redeclared
+
+// var
+var num = 45;
+var num= 34;
+console.log(num); // can be redeclared and reassigned
+
+
+// DATA TYPE
+// 1: Primitive data type 
+// N - Number -> Represents all types of numbers (integers, floats, etc.).
+// N - Null -> Represents an intentional absence of any object or value, indicating that a particular memory box is empty.
+// B - Boolean -> Represents logical values: true or false.
+// B - BigInt -> Represents integers larger than Number.MAX_SAFE_INTEGER. It allows for arbitrary precision in numeric operations.
+// S - String -> Represents a sequence of characters enclosed in " ", ' ', or ` ` (template literals).
+// S - Symbol -> Represents a unique and immutable value, often used as a key for object properties.
+// U - Undefined -> Represents a variable that has been declared but not yet assigned a value.
+
+// // 2: Non-primitive data type
+// -Object
+// -function
+// -Array
+// -date
+
+// OPERATOR:
+
+// 1. arithmetic operators: +, -, *, /, %, **
+// 2. assignment operators: =, +=, -=, ++, --, *=, /=, **=, %=
+// 3. comparison operators: ==, !=, ===, !==, >, <, >=, <=
+// 4. logical operators: &&, ||,!, &amp;&amp;
+// 5. bitwise operators: &, |, ^, ~, <<, >>, &amp;&amp;&amp;
+
+
+console.log("Sneha" -  4); //NaN
+console.log(true + 4); // output 5 true=1
+console.log(false + 4); //output 4 false=0
+
