706ee54c39e9cf92fe5a33f58739cc13f3889557
SnehaChaursia <chaurasiasneha30@gmail.com>
2025-04-26 17:34:34 +0530

    mern

--- SEM2/MERN/NodeJSExpress/Express/express1/index.js
+++ SEM2/MERN/NodeJSExpress/Express/express1/index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app=express();
+const fs = require('fs');
 const Users = require('./MOCK_DATA.json');
+app.use(express.urlencoded({extended:false}));
+app.use((req,res,next)=>{
+    console.log("hello from midlleware 2")
+    return res.json({status:"success"})
+    next();
+
+})
 app.get("/api/Users",(req,res)=>{
    return res.json(Users);
 })
@@ -11,9 +19,34 @@ app.get("/Users",(req,res)=>{
 app.route("/Users/:id")
 
 .get((req,res)=>{
-    const id=Number(req.params.id);
+    const id=Number(req.params.id);  // params  is used for getting the id from the url
     const userid=Users.find((user)=>user.id===id);
     return res.json(userid);
 })
+
+// to create new user
+app.post("/api/Users/",(req,res)=>{
+    const body=req.body;
+    // console.log("body",body);
+    Users.push({id:Users.length+1,...body});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(Users),(err)=>{
+        if(err){
+            console.log("Error",err)
+        }else{
+            console.log("File created")
+        }
+    })
+ return  res.json({status:"success",id:Users.length+1})
+});
+// to update user with  id
+app.patch("/api/Users/:id",(req,res)=>{
+    return  res.json({status:"Pending"})
+   });
+//    to delete the user with id    
+   app.delete("/api/Users/:id",(req,res)=>{
+    return  res.json({status:"Pending"})
+   });
+
+
 const PORT=8000;
 app.listen(PORT,()=>(console.log("Server is running")));
